9602e7c6ace6c67c172843304f75572a2b7a7b60
ssuciu <sorin.suciu@8x8.com>
2018-07-24 08:08:32 +0300

    Adds Spring Boot with swagger and actuator

--- src/main/java/world/Application.java
+++ src/main/java/world/Application.java
@@ -1,8 +1,13 @@
 package world;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
 
     }
 }
