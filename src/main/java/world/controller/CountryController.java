34cee55875e87e4a61a060e242aefc44e58aaa17
ssuciu <sorin.suciu@8x8.com>
2018-07-24 08:35:45 +0300

    Adds controller method for CRUD operations on countries

--- src/main/java/world/controller/CountryController.java
+++ src/main/java/world/controller/CountryController.java
@@ -1,21 +1,44 @@
 package world.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import world.model.Country;
 import world.service.CountryService;
 
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 @RestController
+@RequestMapping("/countries")
 public class CountryController {
 
     @Autowired
     CountryService countryService;
 
-    @GetMapping("/countries")
+    @GetMapping("/")
     public Collection<Country> getCountries() {
         return countryService.getCountries();
+    }
+
+    @GetMapping("/{key}")
+    public Country getCountry(final @NotNull @PathVariable(value = "key") String key) {
+        return null;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Country createCountry(final @RequestBody Country Country) {
+        return null;
+    }
+
+    @PutMapping(value = "/{key}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Country updateCountry(final @NotNull @PathVariable(value = "key") String key,
+                                 final @NotNull @RequestBody Country country) {
+        return null;
+    }
+
+    @DeleteMapping(value = "/{key}")
+    public Country deleteCountry(final @NotNull @PathVariable(value = "key") String key) {
+        return null;
     }
 }
